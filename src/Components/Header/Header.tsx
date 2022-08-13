7c11eeb7c6a7c8c94a358ae31046df0f1231ba63
samanenikookar <snikookar1985@gmail.com>
2022-08-13 11:03:12 +0430

    Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,42 @@
 import React from 'react'
+import Link from 'next/link';
+import Styles from '../Header.scss';
+
+
 
 export default function Header() {
-	return <div>Header</div>
-}
+	return(
+		<header>
+		  <div className="nav container">
+			<Link href="/">
+			  <a><h1>E-Shop</h1></a>
+			</Link>
+			<nav>
+			  <Link href="#">
+				<a>Home</a>
+			  </Link>
+			  <Link href="#">
+				<a>Shop</a>
+			  </Link>
+			  <Link href="#">
+			  <a>Elements</a>
+			  </Link>
+			  <Link href="#">
+			  <a>Blog</a>
+			  </Link>
+			  <Link href="#">
+			  <a>About</a>
+			  </Link>
+			  <Link href="#">
+			  <a>Contact</a>
+			  </Link>
+			  
+			</nav>
+		</div>
+	
+			
+		</header>
+	  )
+	};
+	
+	
